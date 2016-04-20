d1ecae093538e22a1a4f9d992c16e4bcb61c1aa6
Michael Pascuzzi <gatorpazz@gmail.com>
2016-04-19 20:53:45 -0400

    trololol

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => <div>Webpack and React are so cool, report cards hooray</div>;
+export default () => <div>Webpack and React are so cool, report cards hooray, also Jru is a fgt</div>;
